e3602c3241e4c8071d301a57139e93b019a13fd6
parshwa-25 <kavy122007@gmail.com>
2026-07-20 00:29:40 +0530

    Login & Registration Done.

---
+++ register.php
@@ -0,0 +1,9 @@
+<?php
+$user = $_POST['uname'];
+$pass = $_POST['pword'];
+$dob = $_POST['dob'];
+
+echo "Username: " . $user . "<br>";
+echo "Password: " . $pass . "<br>";
+echo "Date of Birth: " . $dob;
+?>
---
+++ student.php
@@ -0,0 +1,6 @@
+<?php
+$user = $_POST['uname'];
+$pass = $_POST['pword'];
+
+echo "Username: " . $user . "<br>";
+echo "Password: " . $pass;
